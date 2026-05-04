474cd80737a0b900d17e6b392aa610eb298a7231
Sangameshwar <119867115+Sangamesh080@users.noreply.github.com>
2026-05-04 15:40:38 +0530

    Update email address in config.py

--- flask-todo-app/config.py
+++ flask-todo-app/config.py
@@ -5,4 +5,4 @@ class Config:
     AWS_REGION = 'ap-south-2'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
-    EMAIL = "sbiradar2185@gmail.com"
+    EMAIL = "sgb080.gitam@gmail.com"
